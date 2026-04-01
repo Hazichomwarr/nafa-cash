6207f9ad3a0eb83f01192359c1f2b9c3e694ef3e
hazichomwarr <batchiy6@gmail.com>
2026-03-31 20:26:55 -0400

    feat: add bg-image to success/page.tsx and change favicon

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -3,21 +3,6 @@ import SendMoneyForm from "../_components/forms/SendMoneyForm";
 
 export default function SendPage() {
   return (
-    // <section className="py-12 bg-linear-to-br from-blue-700 to-orange-400 min-h-screen">
-    //   <Container>
-    //     <Stack gap={6}>
-    //       <h1 className="text-3xl font-bold">
-    //         Finaliser votre transfert en moins de 5 minutes
-    //       </h1>
-
-    //       <Card>
-    //         <CardContent>
-    //           <SendMoneyForm />
-    //         </CardContent>
-    //       </Card>
-    //     </Stack>
-    //   </Container>
-    // </section>
     <section className="relative min-screen bg-[url('/images/nafa-bobo.png')] bg-cover bg-center">
       <div className="absolute inset-0 bg-black/40" />
 
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -28,32 +28,34 @@ export default async function SuccessPage({ searchParams }: Props) {
   if (!transaction) return <div>Transaction not found</div>;
 
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-      <div className="bg-white rounded-xl shadow-xl p-8 w-full max-w-md text-center">
-        {/* ICON */}
-        <div className="text-green-600 text-4xl mb-2">✓</div>
+    <section className="relative min-h-screen bg-[url('/images/nafa-logo.png')]  bg-cover bg-center md:bg-left">
+      <div className="absolute inset-0 bg-black/70 flex items-center justify-center">
+        <div className="relative z-10 w-full max-w-md bg-neutral-100 rounded-xl shadow-xl p-8 text-center ">
+          {/* ICON */}
+          <div className="text-green-600 text-4xl mb-2">✓</div>
 
-        {/* TITLE */}
-        <h1 className="text-2xl font-bold">Transfert réussi</h1>
+          {/* TITLE */}
+          <h1 className="text-2xl font-bold">Transfert réussi</h1>
 
-        {/* MESSAGE */}
-        <p className="text-neutral-600 mt-2">
-          {transaction.recipient.name} recevra{" "}
-          <strong>{transaction.amount.toLocaleString()} CFA</strong> dans
-          quelques minutes.
-        </p>
+          {/* MESSAGE */}
+          <p className="text-neutral-600 mt-2">
+            {transaction.recipient.name} recevra{" "}
+            <strong>{transaction.amount.toLocaleString()} CFA</strong> dans
+            quelques minutes.
+          </p>
 
-        {/* BUTTONS */}
-        <div className="flex gap-3 mt-6 justify-center">
-          <Link href="/">
-            <Button variant="secondary">Accueil</Button>
-          </Link>
+          {/* BUTTONS */}
+          <div className="flex gap-3 mt-6 justify-center">
+            <Link href="/">
+              <Button variant="secondary">Accueil</Button>
+            </Link>
 
-          <Link href={`/history?email=${email}`}>
-            <Button>Mes transferts</Button>
-          </Link>
+            <Link href={`/history?email=${email}`}>
+              <Button>Mes transferts</Button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
