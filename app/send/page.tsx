e947a7eed47447d82cffe36f8bced73cbefb6996
hazichomwarr <batchiy6@gmail.com>
2026-04-04 14:48:18 -0400

    update send/page.tsx styling

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -6,7 +6,7 @@ export default function SendPage() {
     <section className="relative min-screen bg-[url('/images/nafa-bobo.png')] bg-cover bg-center">
       <div className="absolute inset-0 bg-black/40" />
 
-      <div className="relative z-10 py-12">
+      <div className="md:w-[50%] mx-auto relative z-10 py-12">
         <Container>
           <Stack gap={2}>
             <h2 className="text-4xl font-bold text-white">
